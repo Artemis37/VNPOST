b621c36bf64f13f0c99fd53d77e4705d042dab5e
artemis37 <xuyenchixanh1976@gmail.com>
2021-09-30 04:29:46 +0700

    Home page

---
+++ VNPOST/Content/js/Index.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+$(document).ready(function () {
+    //indicator show on hover
+    $("#carouselImageIndicators").mouseenter(function () {
+        $(".carousel-indicators").css({ "visibility": "visible", "opacity": "1" });
+        $(".carousel-control-hover").css({ "visibility": "visible", "opacity": "1" });
+    });
+    $("#carouselImageIndicators").mouseleave(function () {
+        $(".carousel-indicators").css({ "visibility": "hidden", "opacity": "0" });
+        $(".carousel-control-hover").css({ "visibility": "hidden", "opacity": "0" });
+    });
+
+    //carousel news
+    $(".first-five-news-item:nth-child(1)").css("background-color", "#fcb71e");
+    let i = 1;
+    setInterval(() => {
+        $(`.first-five-news-item:nth-child(${i})`).css("background-color", "inherit");
+        i++;
+        if (i > 5) i = 1;
+        $(`.first-five-news-item:nth-child(${i})`).css("background-color", "#fcb71e");
+        if (i === 4) {
+            $("#preview-news div img").attr("src", `/Content/Images/News/${i}.png`);
+        } else {
+            $("#preview-news div img").attr("src", `/Content/Images/News/${i}.jpg`);
+        }
+    }, 10000);
+});
